15ef620a670190b27a1e1b70981817166cec9635
Paulo Rodriguez <paulo_crr@hotmail.com>
2020-05-02 18:19:24 -0500

    Mecanicas de juego terminadas

---
+++ Assets/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator animator;
+    private Vector3 originalPos;
+    public bool walk = false;
+    public string type;
+    public int life = 3;
+    public string markerId;
+    public GameObject lifeBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetInteger("battle", 1);
+        animator.SetInteger("moving", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (walk) {
+            transform.Translate((Vector3.forward * Time.deltaTime) / 3);
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        Debug.Log(obj.tag);
+        if (obj.CompareTag("Power"))
+        {
+            Debug.Log("Entro20");
+            life--;
+            Destroy(obj);
+
+            if (life <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+            else {
+                lifeBar.transform.localScale = new Vector3(lifeBar.transform.localScale.x - 0.9f, lifeBar.transform.localScale.y, lifeBar.transform.localScale.z);
+                StartCoroutine(playHitAnimation());
+            }
+
+        }
+
+       
+
+    }
+
+    IEnumerator playHitAnimation()
+    {
+        animator.SetInteger("moving", 11);
+
+        yield return new WaitForSeconds(0.1f);
+        animator.SetInteger("battle", 1);
+        animator.SetInteger("moving", 2);
+       
+    }
+
+}
--- Assets/Scripts/Dinamico.cs
+++ Assets/Scripts/Dinamico.cs
@@ -5,92 +5,77 @@ using UnityEngine;
 using System.Text;
 using UnityEngine.Windows.Speech;
 using System;
-using Vuforia;
 
 
-public class Dinamico : MonoBehaviour, ITrackableEventHandler
+public class Dinamico : MonoBehaviour
 {
     /*parte voz*/
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, int> actions = new Dictionary<string, int>();
+    public ConfidenceLevel confidence = ConfidenceLevel.Low;
     /*termina*/
 
-    private TrackableBehaviour mTrackableBehaviour;
-
     public GameObject[] userPowers;
-    private int actualPowerIndex = 0;
-
+    private int actualPowerIndex = 1;
+    private bool shoot = false;
+    private GameObject shootPower;
+    Vector3 startPosition;
 
 
     // Use this for initialization
     void Start()
     {
 
-        actions.Add("fire", 0);
-        actions.Add("water", 1);
-        actions.Add("earth", 2);
-        actions.Add("wind", 3);
-        userPowers[actualPowerIndex].active = true;
-        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        actions.Add("fuego", 0);
+        actions.Add("agua", 1);
+        actions.Add("tierra", 2);
+        actions.Add("disparar", 3);
+        userPowers[actualPowerIndex].SetActive(true);
+        startPosition = new Vector3(userPowers[actualPowerIndex].transform.position.x, userPowers[actualPowerIndex].transform.position.y, userPowers[actualPowerIndex].transform.position.z);
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray(),confidence);
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
         keywordRecognizer.Start();
 
-
-        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-
-        if (mTrackableBehaviour)
-        {
-            mTrackableBehaviour.RegisterTrackableEventHandler(this);
-        }
+        startPosition = new Vector3(userPowers[1].transform.position.x, userPowers[1].transform.position.y, userPowers[1].transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown("u"))
+            shoot = true;
+        if (shoot)
         {
-            print("space key was pressed");
-            palabra = "Prefabs/agua";
+            shootPower = Instantiate(userPowers[actualPowerIndex]);
+            if (actualPowerIndex == 0)
+            {
+                var main = shootPower.GetComponent<ParticleSystem>().main;
+                main.scalingMode = ParticleSystemScalingMode.Local;
+            }
+            shootPower.transform.position = new Vector3(userPowers[actualPowerIndex].transform.position.x + 1, userPowers[actualPowerIndex].transform.position.y, userPowers[actualPowerIndex].transform.position.z);
+            shootPower.GetComponent<ShootPower>().move = shoot;
+            shoot = false;
 
-        }*/
+        }
 
     }
 
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
     {
         Debug.Log(speech.text);
-        userPowers[actualPowerIndex].active = false;
-        actualPowerIndex = actions[speech.text];
-        userPowers[actualPowerIndex].active = true;
-    }
-
-
-
-
-    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
-    {
-        if (newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        if (actions[speech.text] == 3)
         {
-           
             
-                OnTrackingFound();
             
+            shoot = true;
+
         }
-        else
+        else if(actions.ContainsKey(speech.text))
         {
-            OnTrackingLost();
+            userPowers[actualPowerIndex].SetActive(false);
+            actualPowerIndex = actions[speech.text];
+            userPowers[actualPowerIndex].SetActive(true);
         }
-    }
-
-    private void OnTrackingLost()
-    {
-
-    }
-
-    private void OnTrackingFound()
-    {
-
-        Debug.Log("Image Target Found!");
         
     }
 
